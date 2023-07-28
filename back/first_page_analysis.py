84e88fd3a7df8cfb50ac1ab8f96e8493b2ebdddc
aleksandr88p <aleksandr88p@gmail.com>
2023-07-28 20:34:51 +0200

    5 new video

--- back/first_page_analysis.py
+++ back/first_page_analysis.py
@@ -41,7 +41,7 @@ response = requests.get(url2, headers=headers, proxies=proxies)
 
 
 # with open('page.html', 'a') as f:
-#     f.write(response.text)
+#     f.write(response.text)nickiminaj
 
 # with open('page.html', 'r') as f:
 #     soup = BeautifulSoup(f.read(), 'lxml')
@@ -68,6 +68,9 @@ microformat
 /header/c4TabbedHeaderRenderer/videosCountText/runs/0/text
 /header/c4TabbedHeaderRenderer/subscriberCountText/accessibility/accessibilityData/label
 /header/c4TabbedHeaderRenderer/subscriberCountText/simpleText
+
+/contents/twoColumnBrowseResultsRenderer/tabs/1/tabRenderer/content/richGridRenderer/contents/0/richItemRenderer/content/videoRenderer/title/runs/0/text
+/contents/twoColumnBrowseResultsRenderer/tabs/1/tabRenderer/content/richGridRenderer/contents/1/richItemRenderer/content/videoRenderer/title/runs/0/text
 """
 
 # print(json_str)
@@ -92,3 +95,79 @@ print(channel_id)
 print(subscr_count)
 print(videos_count)
 print(keywords_from_meta)
+
+##################################
+# looking for continuation token #
+##################################
+
+"""
+/contents/twoColumnBrowseResultsRenderer/tabs/1/tabRenderer/content/richGridRenderer/contents/30/continuationItemRenderer/continuationEndpoint/continuationCommand/token
+
+"""
+
+def find_key(input_dict, target_key):
+    # if target_key in input_dict:
+    #     yield input_dict[target_key]
+    # elif isinstance(input_dict, dict):
+    #     for k, v in input_dict.items():
+    #         if isinstance(v, (dict, list)):
+    #             yield from find_key(v, target_key)
+    # elif isinstance(input_dict, list):
+    #     for item in input_dict:
+    #         yield from find_key(item, target_key)
+    found_values = []
+    if target_key in input_dict:
+        found_values.append(input_dict[target_key])
+    elif isinstance(input_dict, dict):
+        for k, v in input_dict.items():
+            if isinstance(v, (dict, list)):
+                found_values.extend(find_key(v, target_key))
+    elif isinstance(input_dict, list):
+        for item in input_dict:
+            found_values.extend(find_key(item, target_key))
+    return found_values
+
+
+def get_value_by_path(data, path):
+    """Вспомогательная функция для извлечения значения из словаря по заданному пути."""
+    for key in path:
+        if isinstance(data, dict):
+            data = data.get(key)
+        elif isinstance(data, list):
+            data = data[int(key)]
+        else:
+            return None
+    return data
+
+def find_video_titles(data, max_titles=5):
+    """Функция для извлечения заголовков видео из данных JSON."""
+    path_to_titles = [
+        "contents", "twoColumnBrowseResultsRenderer", "tabs", 1, "tabRenderer",
+        "content", "richGridRenderer", "contents"
+    ]
+
+    titles = []
+    try:
+        contents = get_value_by_path(data, path_to_titles)
+        for content in contents:
+            title_path = ["richItemRenderer", "content", "videoRenderer", "title", "runs", 0, "text"]
+            title = get_value_by_path(content, title_path)
+            if title:
+                titles.append(title)
+            if len(titles) >= max_titles:
+                break
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return titles
+
+
+a = find_key(input_dict=data, target_key='continuationCommand')
+
+for i in a:
+    print(i['token'])
+
+
+
+
+titles = find_video_titles(data)
+print(titles)
