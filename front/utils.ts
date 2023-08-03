e2ab024577d9e23bb2751d39a1350f411afbfc1b
Alexander <tipkrut9@gmail.com>
2023-08-03 18:29:42 +0200

    integrated chat gpt

--- front/utils.ts
+++ front/utils.ts
@@ -1,4 +1,21 @@
+import { Configuration, OpenAIApi } from 'openai';
+
 export function isYouTubeLink(url: string): boolean {
   const pattern = /^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/;
   return pattern.test(url);
+}
+
+export async function getKeywords(textArray: Array<string>) {
+  const configuration = new Configuration({
+    apiKey: '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',
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const text = textArray.join(', ');
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{"role": "system", "content": "You are a helpful assistant."}, {role: "user", content: `Вот мой список заголовков: ${text}. Пожалуйста, выделите 10 ключевых слов из этого списка, которые можно использовать для поиска на YouTube.`}],
+  });
+  console.log(completion.data.choices[0].message);
 }
