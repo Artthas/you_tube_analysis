0910da53f7b9ce5ff61e08e0fbf91caabaaba3ed
aleksandr88p <aleksandr88p@gmail.com>
2023-08-03 19:21:27 +0200

    async gpt

--- back/chat_gpt_api.py
+++ back/chat_gpt_api.py
@@ -1,15 +1,38 @@
+# import os
+# import openai
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+#
+# def get_keywords(text_array):
+#   text = ', '.join(text_array)
+#
+#   completion = openai.ChatCompletion.create(
+#     model="gpt-3.5-turbo",
+#     messages=[
+#       {"role": "system", "content": "You are a helpful assistant."},
+#       {"role": "user", "content": f"Вот мой список заголовков: {text}. Пожалуйста, выделите 10 ключевых слов из этого списка, которые можно использовать для поиска на YouTube."}
+#     ]
+#   )
+#   print(completion.choices[0].message)
+
 import os
 import openai
+from aiohttp import ClientSession
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def get_keywords(text_array):
-  text = ', '.join(text_array)
+async def get_keywords(text_array):
+    text = ', '.join(text_array)
 
-  completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      {"role": "system", "content": "You are a helpful assistant."},
-      {"role": "user", "content": f"Вот мой список заголовков: {text}. Пожалуйста, выделите 10 ключевых слов из этого списка, которые можно использовать для поиска на YouTube."}
-    ]
-  )
-  print(completion.choices[0].message)
+    completion = await openai.ChatCompletion.acreate(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": f"Вот мой список заголовков: {text}. Пожалуйста, выделите 10 ключевых слов из этого списка, которые можно использовать для поиска на YouTube."}
+        ]
+    )
+    print(completion.choices[0].message)
+
+    # В конце вашей программы закройте HTTP-сессию
+    openai.session.set(ClientSession())
+
+    await openai.session.get().close()
