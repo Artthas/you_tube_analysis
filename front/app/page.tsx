dae4f53c4435f71134263099ee33cb71e58ba38b
Alexander <tipkrut9@gmail.com>
2023-08-08 14:34:27 +0200

    small edit

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,11 +1,14 @@
 import SearchBar from '@/components/search-bar';
 import styles from '../styles/home.module.scss';
+import SearchResult from '@/components/search-result';
 
 export default function Home() {
   return (
     <main className={styles['main']}>
       
       <SearchBar/>
+
+      <SearchResult/>
 
     </main>
   )
