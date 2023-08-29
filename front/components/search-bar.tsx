ed9be571d2870432684ad1759779866445c2db29
Alexander <tipkrut9@gmail.com>
2023-08-29 18:03:57 +0200

    small edit

--- front/components/search-bar.tsx
+++ front/components/search-bar.tsx
@@ -24,7 +24,7 @@ export default function SearchBar() {
   const handleFormSubmit = async (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
 
-    simulateReqServer(dispatch, mockData);
+    // simulateReqServer(dispatch, mockData);
 
     try {
       dispatch({ type: 'SET_SEARCH_RESULT', payload: {
